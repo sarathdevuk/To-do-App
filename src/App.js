d49b604853b14b6782339f16cf6293ff01fdd26f
Sarathdev <sarathdev0@gmail.com>
2023-04-01 12:58:58 +0530

    style added

--- src/App.js
+++ src/App.js
@@ -1,73 +1,113 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
   const [toDos, setToDos] = useState([]);
   const [input, setInput] = useState("");
 
+  const addTodo = () => {
+    if (input.trim() !== "") {
+      setToDos([...toDos, { id: Date.now(), text: input, status: false }]);
+      localStorage.setItem(
+        "input",
+        JSON.stringify([
+          ...toDos,
+          { text: input, id: Date.now(), status: false },
+        ])
+      );
+      setInput("");
+    }
+  };
 
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+    if (localStorage.getItem("input")) {
+      //key from handlesum
+      setToDos(JSON.parse(localStorage.getItem("input")));
+    }
+    // todoInputRef.current.style.color="red"
+  }, []);
 
+  function deleteTodo(id) {
+    let newTodo = toDos.filter((items) => {
+      return items.id != id;
+    });
+    setToDos(newTodo);
+    localStorage.setItem("input", JSON.stringify([...newTodo]));
+  }
 
   return (
     <div className="app">
-      <div className="mainHeading">
-        <h1>ToDo List</h1>
-      </div>
-      <div className="subHeading">
-        <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
-      </div>
-      <div className="input">
-        <input
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          type="text"
-          placeholder="🖊️ Add item..."
-        />
-        <i
-          onClick={() =>
-            setToDos([...toDos, { id: Date.now(), text: input, status: false }])
-          }
-          className="fas fa-plus"
-        ></i>
-      </div>
-      <div className="todos">
-        {toDos.map((obj) => {
-          return (
-            <div className="todo">
-              <div className="left">
-                <input
-                  onChange={(e) => {
-                    console.log(e.target.value);
-                    setToDos(
-                      toDos.filter((obj2) => {
-                        if (obj2.id === obj.id) {
-                          obj2.status = e.target.checked;
-                        }
-                        return obj2;
-                      })
-                    );
-                  }}
-                  value={obj.status}
-                  type="checkbox"
-                  name=""
-                  id=""
-                />
-                <p>{obj.text}</p>
+      <div className="border">
+        <div className="mainHeading">
+          <h1>ToDo List</h1>
+        </div>
+        <div className="subHeading">
+          <br />
+          <h2>Have a nice day ... </h2>
+        </div>
+        <div className="input">
+          <input
+            value={input}
+            ref={inputRef}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="🖊️ Add item..."
+          />
+          <i onClick={addTodo} className="fas fa-plus"></i>
+        </div>
+        <div className="todos">
+          {toDos.map((obj) => {
+            return (
+              <div className={ obj.status ? "cross"  :"todo"}>
+                <div className={ obj.status ? "left-cross" : "left"}>
+                  <input
+                    onChange={(e) => {
+                      console.log(e.target.value);
+                      setToDos(
+                        toDos.filter((obj2) => {
+                          if (obj2.id === obj.id) {
+                            obj2.status = e.target.checked;
+                  
+                          }
+                          return obj2;
+                        })
+                      );
+                    }}
+                    value={obj.status}
+                    type="checkbox"
+                    name=""
+                    id=""
+                  />
+                  <p>{obj.text}</p>
+                </div>
+                <div className="right">
+                  <i
+                    onClick={() => deleteTodo(obj.id)}
+                    className="fas fa-times"
+                  ></i>
+                </div>
               </div>
-              <div className="right">
-                <i className="fas fa-times"></i>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
 
-        {toDos.map((obj) => {
-          if (obj.status) {
-            return <h1>{obj.text}</h1>;
-          }
-          return null;
-        })}
+          {/* <br />
+          <h2>Completed Tasks</h2>
+          <br />
+          {toDos.map((obj) => {
+            if (obj.status) {
+              return (
+                <div>
+                  <ul className="cmp">
+                    <li>{obj.text}</li>
+                  </ul>
+                </div>
+              );
+            }
+            return null;
+          })} */}
+        </div>
       </div>
     </div>
   );
